4859a9c5db16091f74e19ec3c802758b44856533
Your Name <you@example.com>
2017-08-23 13:52:58 +0800

    api result

--- src/main/java/com/wyjf/app/api/WalletController.java
+++ src/main/java/com/wyjf/app/api/WalletController.java
@@ -14,22 +14,22 @@ import java.util.List;
  * Created by dannis on 2017/8/22.
  */
 @RestController
-@RequestMapping(value="/api/wallet")
+@RequestMapping(value = "/api/wallet")
 @Api(description = "钱包相关操作")
 public class WalletController {
 
-    @ApiOperation(value="测试", notes="测试操作")
-    @RequestMapping(value={"/test"}, method= RequestMethod.GET)
+    @ApiOperation(value = "测试", notes = "测试操作")
+    @RequestMapping(value = {"/test"}, method = RequestMethod.GET)
     public List<String> test() {
         List<String> r = Arrays.asList("Buenos Aires", "你好Córdoba", "La Plata");
         return r;
     }
 
-    @ApiOperation(value="测试2", notes="测试操作")
-    @RequestMapping(value={"/test"}, method= RequestMethod.POST)
-    public List<String> test2() {
+    @ApiOperation(value = "测试2", notes = "测试操作")
+    @RequestMapping(value = {"/test"}, method = RequestMethod.POST)
+    public ApiResult test2() {
         List<String> r = Arrays.asList("1", "2", "3");
-        return r;
+        return ApiResultFactory.createResult(r);
     }
 
 }
