99a4f1dc64fab0028c029f71b294aded6e6f232d
Your Name <you@example.com>
2017-09-12 10:52:33 +0800

    jquery form add.

--- app/src/main/java/com/wyjf/app/web/admin/DrawController.java
+++ app/src/main/java/com/wyjf/app/web/admin/DrawController.java
@@ -1,11 +1,24 @@
 package com.wyjf.app.web.admin;
 
+import com.wyjf.common.domain.Draw;
+import com.wyjf.common.domain.User;
+import com.wyjf.common.message.AjaxPageRequest;
+import com.wyjf.common.repository.DrawRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by Administrator on 2017/9/5.
@@ -16,20 +29,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class DrawController {
     private static final Logger log = LoggerFactory.getLogger(DrawController.class);
 
-    @RequestMapping("/list")
-    public String list() {
-        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        String username = null;
-        if (principal instanceof UserDetails) {
-            log.info("principal instanceof UserDetails");
-            username = ((UserDetails) principal).getUsername();
-        } else {
-            username = principal.toString();
-        }
-        log.info("当前登陆用户:{}", username);
+    @Autowired
+    private DrawRepo drawRepo;
 
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
+    public String list() {
         return "admin/draw/list";
     }
 
+    @RequestMapping(value = "/list", method = RequestMethod.POST)
+    @ResponseBody
+    public Map<Object, Object> listQuery(@ModelAttribute AjaxPageRequest req) {
+        List<Draw> users = drawRepo.findAll();
+        HashMap map = new HashMap();
+        map.put("data", users);
+        map.put("draw", req.getDraw());
+        map.put("recordsTotal", users.size());
+        map.put("recordsFiltered", users.size());
+        return map;
+    }
 
 }
--- common/src/main/java/com/wyjf/common/repository/DrawRepo.java
+++ common/src/main/java/com/wyjf/common/repository/DrawRepo.java
@@ -2,10 +2,11 @@ package com.wyjf.common.repository;
 
 import com.wyjf.common.domain.Draw;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 
 /**
  * Created by Administrator on 2017/9/1.
  */
-public interface DrawRepo extends JpaRepository<Draw, Long> {
+public interface DrawRepo extends JpaRepository<Draw, Long>,QueryDslPredicateExecutor<Draw> {
 
 }
