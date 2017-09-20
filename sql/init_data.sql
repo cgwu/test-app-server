b98b96ee51b39c60560984a79923c4799bc4f0bd
Your Name <you@example.com>
2017-09-20 18:04:00 +0800

    init data.

--- sql/init_data.sql
+++ sql/init_data.sql
@@ -5,3 +5,4 @@ insert into admin (name, password, enabled) values ('root','a665a45920422f9d417e
 
 INSERT INTO `system_param` (`id`, `data_key`, `data_val`, `comment`, `sort_id`, `pid`) VALUES ('1', 'STR_SystemName', '无忧金融', '系统名称', '0', '0');
 INSERT INTO `system_param` (`id`, `data_key`, `data_val`, `comment`, `sort_id`, `pid`) VALUES ('2', 'INT_BeforeBuyMins', '1', '开盘前多长时间内不可买票(分钟)', '0', '0');
+INSERT INTO `system_param` (`id`, `data_key`, `data_val`, `comment`, `sort_id`, `pid`) VALUES ('3', 'INT_PlatformPercent', '20', '平台收益百分比(%)', '0', '0');
